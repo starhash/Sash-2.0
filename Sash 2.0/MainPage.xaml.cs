25446510c7e0a3c3b6c95a5c2314b4b33000ddf1
Harsh Srivastava <harshsri170995@outlook.com>
2016-07-03 11:15:04 +0530

    Add project files.

---
+++ Sash 2.0/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Sash_2._0
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void go_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void close_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WebPageTab_NavigationCompleted_1(WebView sender, WebViewNavigationCompletedEventArgs e)
+        {
+            string title = sender.DocumentTitle;
+            home.Header = (title.Trim().Length == 0)?"";
+        }
+
+        private void WebPageTab_NavigationFailed(object sender, WebViewNavigationFailedEventArgs e)
+        {
+            home.Header = ((WebView)sender).DocumentTitle;
+        }
+
+        private void WebPageTab_NavigationStarting(WebView sender, WebViewNavigationStartingEventArgs e)
+        {
+            home.Header = sender.DocumentTitle;
+        }
+    }
+}
---
+++ Sash 2.0/WebPageTab.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Sash_2._0
+{
+    public sealed partial class WebPageTab : UserControl
+    {
+        public delegate void NavigationStartingHandler(WebView sender, WebViewNavigationStartingEventArgs e);
+        public event NavigationStartingHandler NavigationStarting;
+        public delegate void NavigationCompletedHandler(WebView sender, WebViewNavigationCompletedEventArgs e);
+        public event NavigationCompletedHandler NavigationCompleted;
+        public delegate void NavigationFailedHandler(object sender, WebViewNavigationFailedEventArgs e);
+        public event NavigationFailedHandler NavigationFailed;
+
+        public WebView Browser;
+
+        public WebPageTab()
+        {
+            this.InitializeComponent();
+            Browser = webview;
+            NavigationCompleted += WebPageTab_NavigationCompleted;
+            NavigationFailed += WebPageTab_NavigationFailed;
+            NavigationStarting += WebPageTab_NavigationStarting;
+        }
+
+        void WebPageTab_NavigationStarting(WebView sender, WebViewNavigationStartingEventArgs e)
+        {
+            
+        }
+        void WebPageTab_NavigationFailed(object sender, WebViewNavigationFailedEventArgs e)
+        {
+            
+        }
+        void WebPageTab_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs e)
+        {
+            
+        }
+        private void webview_NavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
+        {
+            NavigationStarting(sender, args);
+        }
+        private void webview_NavigationFailed(object sender, WebViewNavigationFailedEventArgs e)
+        {
+            NavigationFailed(sender, e);
+        }
+        private void webview_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            address.Text = webview.Source.OriginalString;
+            NavigationCompleted(sender, args);
+            webview.Focus(FocusState.Keyboard);
+            webview.Focus(FocusState.Pointer);
+        }
+        private void goaddress_Click(object sender, EventArgs e)
+        {
+            string uri = address.Text;
+            if (!uri.StartsWith("http://"))
+                uri = "http://" + uri;
+            webview.Navigate(new Uri(address.Text, UriKind.Absolute));
+        }        
+    }
+}
